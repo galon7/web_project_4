93569185f1e3dfdfab90b9e7a63ac08441dc4f63
Alon <agoykman@gmail.com>
2021-08-21 11:25:34 +0300

    feature/complete

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 const editBtn = document.querySelector(".profile__info-edit");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".modal__close");
-const form = document.querySelector(".model__form");
+const form = document.querySelector(".modal__form");
+const title = document.querySelector(".form__input-name");
+const subtitle = document.querySelector(".form__input-profession");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
+function resetForm() {
+  title.value="";
+  subtitle.value="";
+}
 
 editBtn.addEventListener("click", function() {
   modal.classList.add("modal_open");
+  title.value = profileTitle.textContent;
+  subtitle.value = profileSubtitle.textContent;
 })
 
 closeBtn.addEventListener("click", function() {
   modal.classList.remove("modal_open");
+  resetForm();
 })
 
-closeBtn.addEventListener("submit", function(e) {
+form.addEventListener("submit", function(e) {
   e.preventDefault();
+  profileTitle.textContent = title.value;
+  profileSubtitle.textContent = subtitle.value;
+  modal.classList.remove("modal_open");
+  resetForm();
 })
+
