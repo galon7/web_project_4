6a38c6c90c28ae4f52ac3f49bb140e6b66885450
Alon <agoykman@gmail.com>
2021-08-28 12:08:14 +0300

    add initial cards

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ const title = document.querySelector(".modal__input_field_name");
 const subtitle = document.querySelector(".modal__input_field_profession");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
+const elementsTemplate = document.querySelector("#elements__item").content;
+const elements = document.querySelector(".elements");
 
 function resetForm() {
   title.value="";
@@ -33,3 +35,38 @@ form.addEventListener("submit", function(e) {
   resetForm();
 })
 
+
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+initialCards.forEach(function (item) {
+  const element = elementsTemplate.querySelector('.elements__item').cloneNode(true);
+  element.querySelector(".elements__img").src = item.link;
+  element.querySelector(".elements__title").textContent = item.name;
+  elements.append(element);
+})
